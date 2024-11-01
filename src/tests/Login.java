ec8e53339985cfc8df85ba653e15cd9b0666a449
Omer8389 <omrk1417@gmail.com>
2024-11-01 20:59:25 +0300

    add Login.java.extetion

---
+++ src/tests/Login.java
@@ -0,0 +1,16 @@
+package tests;
+
+import javax.swing.*;
+
+public class Login {
+
+     public static void main(String[] args) {
+
+         int x=10;
+         int y=12;
+
+         System.out.println("result:"+(x+y));
+
+    }
+
+}
